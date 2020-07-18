b2f59cb0fe139de4a5370382932c715dffb2b77e
Rafał Klinowski <klinowskirafal@gmail.com>
2020-07-18 23:43:53 +0200

    Add Singleton

---
+++ Creational/Singleton.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.Creational
+{
+	// A class of which only a single instance can exist
+	internal class Singleton
+	{
+		// This is the unique instance - only one exists at a time
+		private static Singleton _instance;
+
+		// The following property makes sure only one instance exists
+		public static Singleton Instance
+		{
+			get
+			{
+				// If the instance is null (the property is called for the first time) - create a new one
+				if (_instance is null)
+				{
+					_instance = new Singleton();
+				}
+
+				// Return the instance
+				return _instance;
+			}
+		}
+
+		public Singleton()
+		{
+		}
+	}
+}
