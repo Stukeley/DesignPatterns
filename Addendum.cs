cd5f3ef15db2254637f47303d3ec8be629062173
Rafał Klinowski <klinowskirafal@gmail.com>
2020-07-19 13:52:34 +0200

    Add Bridge. Add a short cheatsheet that compares 'abstract class' and 'interface'.

---
+++ Addendum.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns
+{
+	internal class Addendum
+	{
+		//! Main differences between 'abstract class' and 'interface'
+
+		// Interface:	 does not contain a constructor
+		// Abstract class: CAN contain a constructor
+
+		// Interface: does not contain static methods
+		// Abstract class: CAN contain static methods
+
+		// Interface: can only contain methods and properties
+		// Abstract class: can ALSO contain fields, constants
+
+		// Interface: a class can implement many interfaces
+		// Abstract class: a class can only implement ONE other class
+	}
+}
---
+++ Structural/Bridge.cs
@@ -0,0 +1,67 @@
+﻿namespace DesignPatterns.Structural.Bridge
+{
+	// Separates an object’s interface from its implementation
+
+	// Connects with the abstract Implementor
+	public class Abstraction
+	{
+		// Limited access
+		public Implementor Implementor { protected get; set; }
+
+		public virtual void Operation()
+		{
+			Implementor.Operation();
+		}
+	}
+
+	// Provides primitive operations for implementation classes
+	public abstract class Implementor
+	{
+		public abstract void Operation();
+	}
+
+	// Extends Abstraction, including some additional functionality
+	public class RefinedAbstraction : Abstraction
+	{
+		public override void Operation()
+		{
+			base.Operation();
+
+			// Do something else here
+		}
+	}
+
+	// Defines core operation of the implementor
+	public class ConcreteImplementorA : Implementor
+	{
+		public override void Operation()
+		{
+			// Do something here
+		}
+	}
+
+	// Defines a different core operation of the implementor
+	public class ConcreteImplementorB : Implementor
+	{
+		public override void Operation()
+		{
+			// Do something else here
+		}
+	}
+
+	public class Client
+	{
+		public static void Create()
+		{
+			var ab = new RefinedAbstraction();
+
+			// Set initial implementation
+			ab.Implementor = new ConcreteImplementorA();
+			ab.Operation();
+
+			// Replace the implementation with a different one (within the same object)
+			ab.Implementor = new ConcreteImplementorB();
+			ab.Operation();
+		}
+	}
+}
